a73a26f6d5e3ff0b617e0277690223ac07542f24
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2018-03-23 12:49:01 +0900

    Add nesc-gcc.rb

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# homebrew-tinyos-msp430
+homebrew-tinyos-msp430
+======================
+
+The [Homebrew][] formulae of [TinyOS for small MSP430 chips][].  This
+repository includes `nesc-gcc`.
+
+To get everything, execute the following commands.
+
+    $ brew tap tgtakaoka/tinyos-msp430
+    $ brew install --HEAD nesc-gcc
+
+Version:
+
+    nesc-gcc HEAD
+
+[Homebrew]: https://brew.sh/
+[TinyOS for small MSP430 chips]: https://github.com/tgtakaoka/tinyos-msp430
---
+++ nesc-gcc.rb
@@ -0,0 +1,43 @@
+class NescGcc < Formula
+  desc "Programming language for deeply networked systems"
+  homepage "https://github.com/tinyos/nesc"
+  head "https://github.com/tinyos/nesc.git"
+
+  if OS.mac?
+    patch do
+      url "https://raw.githubusercontent.com/tgtakaoka/scripts-tinyos-msp430/master/nesc-current-osx_restrict.patch"
+      sha256 "58b2364b67e1c4b78582361149aafe423be6b8e7b5224fc9b446007cf3fe60b6"
+    end
+  end
+  patch do
+    url "https://raw.githubusercontent.com/tgtakaoka/scripts-tinyos-msp430/master/nesc-current-fix_isystem.patch"
+    sha256 "b26fe85550eb2925b07f16a28abb59a6fdeb5620cdbc97ac05e4a286212fb4eb"
+  end
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on :java => :build
+  depends_on "gcc" => :run if OS.mac?
+
+  def install
+    # nesc is unable to build in parallel because multiple emacs instances
+    # lead to locking on the same file
+    ENV.deparallelize
+    ENV.remove_from_cflags(/ ?-I\/usr\/include\/linux/) if OS.linux?
+
+    system "./Bootstrap"
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+
+    if ENV.compiler == :clang
+      gcc = Formula["gcc"]
+      gcc_suffix = gcc.version.to_s.slice(/\d/)
+      gcc_name = gcc.bin/"gcc-#{gcc_suffix}"
+      inreplace bin/"nescc", /\$gcc = ".*";/, "$gcc = \"#{gcc_name}\";"
+    end
+  end
+end
