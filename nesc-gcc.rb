fe97d4dcb737a782834da4a600f1e92790a2ebda
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2019-03-24 16:43:48 +0900

    Remove unnecessary paches

--- nesc-gcc.rb
+++ nesc-gcc.rb
@@ -3,17 +3,6 @@ class NescGcc < Formula
   homepage "https://github.com/tinyos/nesc"
   head "https://github.com/tinyos/nesc.git"
 
-  if OS.mac?
-    patch do
-      url "https://raw.githubusercontent.com/tgtakaoka/scripts-tinyos-msp430/master/nesc-current-osx_restrict.patch"
-      sha256 "58b2364b67e1c4b78582361149aafe423be6b8e7b5224fc9b446007cf3fe60b6"
-    end
-  end
-  patch do
-    url "https://raw.githubusercontent.com/tgtakaoka/scripts-tinyos-msp430/master/nesc-current-fix_isystem.patch"
-    sha256 "b26fe85550eb2925b07f16a28abb59a6fdeb5620cdbc97ac05e4a286212fb4eb"
-  end
-
   depends_on "automake" => :build if OS.mac?
   depends_on "autoconf" => :build if OS.mac?
   depends_on :java => :build
